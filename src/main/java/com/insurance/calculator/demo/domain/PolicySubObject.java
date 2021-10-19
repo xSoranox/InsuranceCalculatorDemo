bed155317a595a942886df49aec7e5857644a0e5
dzuljeta.miglane <dzuljeta.miglane@accenture.com>
2021-10-19 22:30:34 +0300

    Deleted unused import

--- src/main/java/com/insurance/calculator/demo/domain/PolicySubObject.java
+++ src/main/java/com/insurance/calculator/demo/domain/PolicySubObject.java
@@ -1,7 +1,6 @@
 package com.insurance.calculator.demo.domain;
 
 import com.insurance.calculator.demo.enumeration.RiskType;
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.stereotype.Component;
